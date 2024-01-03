af11c41516bf3660004e9f5b3967a7c126cc281e
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2024-01-02 19:07:07 -0600

    added social media logos to footer comonent

--- hidden-brilliance/src/components/Footer.tsx
+++ hidden-brilliance/src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import spotifyLogo from '../resources/spotify-logo.png'
+import spotifyLogo from '../resources/spotifyEmblem.png'
+import appleMusicEmblem from '../resources/appleMusicEmblem.png'
+import youtubeLogo from '../resources/youtubeEmblem.png'
+import igLogo from '../resources/igEmblem.png'
+
 import './Footer.css'; // Import your component-specific CSS
 
 interface SpotifyLinkProps {
@@ -15,7 +19,6 @@ const Footer: React.FC<SpotifyLinkProps> = ({ link, title }) => {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <h5>Footer Section 1</h5>
             <a
   className="site-nav__link js-no-transition"
   target="_blank"
@@ -25,20 +28,23 @@ const Footer: React.FC<SpotifyLinkProps> = ({ link, title }) => {
 >
   <div aria-hidden="true"  role="presentation" className="icon icon-spotify fa-brands">
 {/* TODO: Ask why the CSS is not re-sizing image */}
-  <img src={spotifyLogo} alt="Spotify Logo" className='social-medial-icons' />
+  <img src={spotifyLogo} alt="Spotify Logo" className='social-medial-icons'    // Inline style added here
+ />
+  <img src={appleMusicEmblem} alt="Apple Music Logo" className='social-medial-icons' />
+  <img src={youtubeLogo} alt="YouTube Music Logo" className='social-medial-icons' />
+  <img src={igLogo} alt="Instagram Logo" className='social-medial-icons' />
+
 
   </div>
   <span className="icon__fallback-text">Spotify</span>
 </a>
           </div>
           <div className="col-md-6">
-            <h5>Footer Section 2</h5>
-            <p>Content for the second section of the footer.</p>
           </div>
         </div>
         <div className="row">
           <div className="col-12 text-center">
-            <p>&copy; {new Date().getFullYear()} Your Website</p>
+            <p>&copy; {new Date().getFullYear()} Hidden Brilliance</p>
           </div>
         </div>
       </div>
