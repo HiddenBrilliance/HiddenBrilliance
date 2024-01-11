745870cb61393120a30cc5a3e8cbcd3ffb397555
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2024-01-10 18:52:21 -0600

    aligned blog items

--- hidden-brilliance/src/components/Header.tsx
+++ hidden-brilliance/src/components/Header.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import './Header.css'; 
 import { Navbar, Container, Nav, Button, Form, FormControl } from 'react-bootstrap';
 import hbLogo from '../resources/hbLogo.png'
-// TODO: Figure out why the import is not working
+
 
 
 const Header: React.FC = () => {
   return (
     <Navbar expand="lg" bg="black">
-      <Container>
+      <Container className='nav-align'>
         <Navbar.Toggle aria-controls="navbarTogglerDemo02" />
+        <Navbar.Collapse id="navbarTogglerDemo02">
+          <Nav className="me-auto mb-2 mb-lg-0">
+            <Nav.Link href="#" className='text-color' disabled>Home</Nav.Link>
+            <Nav.Link href="#">Shop</Nav.Link>
+            <Nav.Link href="">Blog</Nav.Link>
+          </Nav>
+
            <Navbar.Brand href="#">
           <img 
             src={hbLogo} 
@@ -19,18 +26,8 @@ const Header: React.FC = () => {
             alt="Logo"
           />
         </Navbar.Brand> 
-        <Navbar.Collapse id="navbarTogglerDemo02">
-          <Nav className="me-auto mb-2 mb-lg-0">
-            <Nav.Link href="#" className='custom-text' active>Home</Nav.Link>
-            <Nav.Link href="#">Shop</Nav.Link>
-            <Nav.Link href="#">Blog</Nav.Link>
-          </Nav>
-
           
-          <Form className="d-flex" role="search">
-            <FormControl type="search" placeholder="Search" aria-label="Search" />
-            <Button variant="outline-success" type="submit">Search</Button>
-          </Form>
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
