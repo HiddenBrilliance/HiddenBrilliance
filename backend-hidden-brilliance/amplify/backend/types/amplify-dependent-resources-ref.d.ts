1125a400a543726a06bd4c04c9c55d607d8f19cf
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2024-01-13 20:13:54 -0600

    build GraphQL endpoint for backend

--- backend-hidden-brilliance/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ backend-hidden-brilliance/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "backendhb": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
